6767c5b2cfb1d3445bc75272ebe6e28679854351
Sammy Jackson <sam.jackson@live.ca>
2022-02-17 17:57:16 -0500

    fixed header nav spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,27 +6,24 @@ import { makeStyles } from "@mui/styles";
 import { isMobile } from 'react-device-detect';
 import '../css/Header.css';
 
-// const useStyles = makeStyles({
-//  	 root: {
-//     	position: "sticky",
-// 		top: "0rem",
-// 		'z-index': 1,
-// 		background: '#000',
-//   	},
+// const NavigationLinks = () => {
+// 	let navigate = useNavigate();
+// 	const location = useLocation();
 
-// 	selected: {
-// 		'border-bottom': '1px solid',
-// 		color: '#ffffff',
-// 		margin: 0,
-// 		display: 'inline-flex',
-// 	},
-
-// 	selectedTransparent: {
-// 		'border-bottom': '1px solid transparent',
-// 		margin: 0,
-// 		display: 'inline-flex',
-// 	}
-// });
+// 	return (
+//     <React.Fragment>
+//       <Grid item xs={4}>
+// 				<h3 className={location.pathname !== '/about' && location.pathname !== '/contact' ? 'selected' : 'selectedTransparent' } onClick={e => navigate('/')} >WORK</h3>
+//       </Grid>
+//       <Grid item xs={4}>
+// 				<h3 className={location.pathname == '/about' ? 'selected' : 'selectedTransparent' } onClick={e => navigate('/about')}>ABOUT</h3>
+//       </Grid>
+//       <Grid item xs={4}>
+// 				<h3 className={location.pathname == '/contact' ? 'selected' :'selectedTransparent' } onClick={e => navigate('/contact')}>CONTACT</h3>
+//       </Grid>
+//     </React.Fragment>
+//   );
+// }
 
 const NavigationLinks = () => {
 	let navigate = useNavigate();
@@ -34,16 +31,12 @@ const NavigationLinks = () => {
 
 	return (
     <React.Fragment>
-      <Grid item xs={4}>
-				<h3 className={location.pathname !== '/about' && location.pathname !== '/contact' ? 'selected' : 'selectedTransparent' } onClick={e => navigate('/')} >WORK</h3>
-      </Grid>
-      <Grid item xs={4}>
-				<h3 className={location.pathname == '/about' ? 'selected' : 'selectedTransparent' } onClick={e => navigate('/about')}>ABOUT</h3>
-      </Grid>
-      <Grid item xs={4}>
-				<h3 className={location.pathname == '/contact' ? 'selected' :'selectedTransparent' } onClick={e => navigate('/contact')}>CONTACT</h3>
-      </Grid>
-    </React.Fragment>
+		<div style={{display: 'inline-block'}}>
+			<h3 className={location.pathname !== '/about' && location.pathname !== '/contact' ? 'selected' : 'selectedTransparent' } onClick={e => navigate('/')} >WORK</h3>
+			<h3 className={location.pathname == '/about' ? 'selected' : 'selectedTransparent' } onClick={e => navigate('/about')}>ABOUT</h3>
+			<h3 className={location.pathname == '/contact' ? 'selected' :'selectedTransparent' } onClick={e => navigate('/contact')}>CONTACT</h3>
+    	</div>
+	</React.Fragment>
   );
 }
 
@@ -54,13 +47,13 @@ const Header = () => {
 		<header className={!isMobile ? 'root' : null}>
 			<Box sx={{ flexGrow: 1 }}>
 				<Grid container spacing={2} alignItems="center" justifyContent="space-between">
-					<Grid item xs={12} sm={6} md={6} className='navTitle left logo'>
+					<Grid item xs={12} sm={5} md={4} className='navTitle left logo'>
 						{/* <div className='pointer'> */}
 						<h2 className='marginZero'><a className='pointer' onClick={e => navigate('/')}>JACKY JACKSON</a></h2>
 						3D MOTION DESIGNER
 						{/* </div> */}
 					</Grid>
-					<Grid container item xs={12} sm={6} md={5} className='navTitle right'>
+					<Grid item xs={12} sm={7} md={8} className='navTitle right'>
 						<NavigationLinks />
 					</Grid>
 				</Grid>
